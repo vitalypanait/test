e89a3450993625adea69682cc8132fa87c5c9f87
vitalypanait <panait.v@yandex.ru>
2016-09-08 22:21:15 +0300

    tets5

--- gulpfile.js
+++ gulpfile.js
@@ -3,44 +3,52 @@
 const gulp = require('gulp');
 
 var exec = require('child_process').exec;
-var path = '/var/www/test/deploy';
-var shared = '/var/www/test/deploy/shared/';
-var repositoryUrl = 'git@github.com:vitalypanait/test.git';
-var now = new Date();
-var newReleaseName = now.getFullYear() + '' + now.getMonth() + '' + now.getDate() + '' + now.getHours() + '' + now.getMinutes() + '' + now.getSeconds();
-var lastCommit;
 
-gulp.task('init', function(cb) {
-	exec('mkdir -p ' + path + ' && mkdir -p ' + path + '/releases && mkdir -p ' + path + '/shared', function (err) {
+var path         = '/var/www/test/deploy/';
+var sharedPath   = path + 'shared/';
+var releasesPath = path + 'releases/';
+var repository   = 'git@github.com:vitalypanait/test.git'
+var branch       = 'master';
+
+
+var releaseName;
+var commit;
+
+function addZero(value) {
+	return (parseInt(value, 10) < 10 ) ? '0' + value : value;
+}
+
+
+gulp.task('deploy:prepare:init', function(cb) {
+	exec('mkdir -p ' + path + ' && mkdir -p ' + releasesPath + ' && mkdir -p ' + sharedPath, function (err) {
 		cb(err);
 	});
 });
 
-gulp.task('last_commit', function(cb) {
-	exec('git ls-remote ' + repositoryUrl +  ' \'master\'', function(err, stdout) {
-		lastCommit = stdout.split('\t')[0];
+gulp.task('deploy:prepare:commit', function(cb) {
+	exec('git ls-remote ' + repository +  ' ' + branch, function(err, stdout) {
+		commit = stdout.split('\t')[0];
 
-		if (lastCommit.length === 0) {
-			cb();
+		if (commit.length === 0) {
+			cb('Empty commit');
 		}
 
 		cb(err);
 	});
 });
 
-gulp.task('git', function(cb) {
-	var cmd = 'if [ -d ' + shared + 'cached-copy ] ; then ' +
-		'cd ' + shared + 'cached-copy && ' +
-		'git remote set-url origin ' + repositoryUrl + ' && ' +
+gulp.task('deploy:prepare:get', function(cb) {
+	var cmd = 'if [ -d ' + sharedPath + 'cached-copy ] ; then ' +
+		'cd ' + sharedPath + 'cached-copy && ' +
+		'git remote set-url origin ' + repository + ' && ' +
 		'git fetch -q origin && ' +
 		'git fetch --tags -q origin && ' +
-		//'git pull -q origin master && ' +
-		'git reset -q --hard ' + lastCommit + ' && ' +
+		'git reset -q --hard ' + commit + ' && ' +
 		'git clean -q -d -f; ' +
-		'else  ' +
-		'git clone -q ' + repositoryUrl + ' ' + shared + 'cached-copy && ' +
-		'cd ' + shared + 'cached-copy && ' +
-		'git checkout -q deploy ' + lastCommit + '; ' +
+		'else ' +
+		'git clone -q ' + repository + ' ' + sharedPath + 'cached-copy && ' +
+		'cd ' + sharedPath + 'cached-copy && ' +
+		'git checkout -q -b deploy ' + commit + '; ' +
 		'fi';
 
 	exec(cmd, function (err) {
@@ -48,25 +56,93 @@ gulp.task('git', function(cb) {
 	});
 });
 
-gulp.task('create-release', function(cb) {
-	exec('cd ' + path + '/releases && mkdir ' + newReleaseName, function(err) {
+gulp.task('deploy:prepare:release', function(cb) {
+	var now         = new Date();
+	var data = [
+		now.getFullYear(),
+		addZero(now.getMonth() + 1),
+		addZero(now.getDate()),
+		addZero(now.getHours()),
+		addZero(now.getMinutes()),
+		addZero(now.getSeconds())
+	];
+
+	releaseName = data.join('');
+
+	exec('cd ' + releasesPath + ' && mkdir ' + releaseName, function(err) {
 		cb(err);
 	})
 });
 
-gulp.task('rsync', function(cb) {
-	exec('rsync -a ' + shared + 'cached-copy/ ' + path + '/releases/' + newReleaseName + '/ --exclude=".git"', function(err) {
+gulp.task('deploy:sync', function(cb) {
+	exec('rsync -a ' + sharedPath + 'cached-copy/ ' + releasesPath + releaseName + '/ --exclude=".git"', function(err) {
 		cb(err);
 	})
 });
 
-gulp.task('link', function(cb) {
-	exec('ln -nfsv ' + path + '/releases/' + newReleaseName + ' ' + path + '/current', function(err) {
+gulp.task('deploy:info', function(cb) {
+	var goToRelease = 'cd ' + releasesPath + releaseName;
+	var cmd         = goToRelease + ' && echo \'' + branch + '\' > ./BRANCH && ' +
+		goToRelease + ' && echo ' + commit + ' > ./REVISION && ' +
+		goToRelease + ' && echo ' + releaseName + ' > ./RELEASE';
+
+	exec(cmd, function(err) {
 		cb(err);
 	})
 });
 
+gulp.task('deploy:symlink', function(cb) {
+	exec('ln -nfsv ' + releasesPath + releaseName + ' ' + path + 'current', function(err) {
+		cb(err);
+	})
+});
 
+var currentRelease;
 
+gulp.task('rollback:find', function(cb) {
+	exec('cat ' + path + 'current/RELEASE ', function(err, stdout) {
+		currentRelease = stdout.split('\n')[0];
 
-gulp.task('deploy', gulp.series('init', 'last_commit', 'git', 'create-release', 'rsync', 'link'));
+		cb(err);
+	});
+});
+
+gulp.task('rollback:get', function(cb) {
+	exec('cd /var/www/test/deploy/releases/ && ls -trd */', function(err, stdout) {
+		var directories = stdout.split('\n');
+
+		if (directories.length === 0) {
+			cb('Empty elder release');
+		}
+
+		var isFindCurrent = false;
+		var rollbackRelease = current;
+
+		directories.some(function(item) {
+			console.log(item);
+			if (isFindCurrent) {
+				rollbackRelease = item;
+
+				return true;
+			}
+
+			if (item === currentRelease) {
+				isFindCurrent = true;
+			}
+		});
+
+		console.log('Rollback release = ' + rollbackRelease);
+
+		cb(err);
+	});
+});
+
+gulp.task('deploy', gulp.series(
+	'deploy:prepare:init',
+	'deploy:prepare:commit',
+	'deploy:prepare:get',
+	'deploy:prepare:release',
+	'deploy:sync',
+	'deploy:info',
+	'deploy:symlink'
+));
